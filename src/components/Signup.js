793fee95faf2348d6eaf4847be304eb31108dd68
Essyuge <ugeesther@gmail.com>
2022-11-11 12:19:26 +0300

    signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -14,3 +14,51 @@ function onchange(e){
   });
 
 }
+function handleSubmit(e){
+    e.preventDefault()
+    fetch('https://find-a-motel.herokuapp.com/signup', {
+      method: 'POST',
+      body: JSON.stringify(
+        signUp
+      ),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+      })
+      .then(res=>res.json())
+      .then(data=>{
+        if(data.id===undefined){
+         return navigate("/")
+        }else
+       return navigate(`/owner/${parseInt(data.id)}/rentals`)
+      
+      })
+    
+    
+    }
+    
+    
+      return (
+        <div>
+       <nav className="cartNav" >
+            <div><Link to="/">Home</Link></div>
+            <p style={{fontStyle:"italic",fontSize:"3rem"}}>Find A Motel</p>
+        </nav>
+    
+        <form  className="signupform" style={{paddingTop:"6rem"}} onSubmit={handleSubmit}>
+        <label htmlFor="name"   value="name">Name:   </label>
+        <input type="text" name="name" onChange={onchange}/><br/><br/>
+        <label htmlFor="email" value="email">Email: </label>
+        <input type="email" name="email" onChange={onchange}/><br/><br/>
+        <label htmlFor="tel" value="tel">Tel: </label>
+        <input type="tel" name="tel" onChange={onchange}/><br/><br/>
+        <label htmlFor="password" value="password" >Password:</label>
+        <input type="password"  name="password" onChange={onchange}/><br/>
+        <input type="submit"  style={{marginTop:"2rem",padding:"15px",border:"none",background:"blue"}}   value="SignUp"/>
+      </form>
+      
+      </div>
+      )
+    }
+    
+    export default Signup;

744dabcc3a6510e77e26f153fb7f2c82c6d8c7a3
Essyuge <ugeesther@gmail.com>
2022-11-11 12:08:21 +0300

    updates

--- src/components/AddRentals.js
+++ src/components/AddRentals.js
@@ -19,3 +19,27 @@ function AddRentals({handleAdd}) {
       });
     
     }
+
+    function handleSubmit(e){
+        e.preventDefault()
+       console.log(add)
+       fetch('https://find-a-motel.herokuapp.com/rentals', {
+    method: 'POST',
+    body: JSON.stringify(
+      add
+    ),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      handleAdd(data)
+      if(data.id===undefined){
+       return navigate("/")
+      }else
+     return navigate(`/owner/${parseInt(owner_id)}/rentals`)
+    })
+          
+      }
+  

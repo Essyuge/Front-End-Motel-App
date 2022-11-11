5d16a8f9f82dd514416054f4b3da9eebfcfd82e5
Essyuge <ugeesther@gmail.com>
2022-11-11 12:15:26 +0300

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,3 +22,24 @@ fetch(`https://find-a-motel.herokuapp.com/login/${email}`)
   navigate(`/owner/${data.id}/rentals`)})
 
 }
+
+
+return (
+    <>
+     <nav className="cartNav">
+        <div><Link to="/">Home</Link></div>
+        <div><Link to="/signup">Signup</Link></div>
+        <p style={{fontStyle:"italic",fontSize:"3rem"}}>Find A Motel</p>
+       </nav>
+    <div className="login">
+    <form  className="form" onSubmit={handleUser}>
+       <label htmlFor="email" value="Email">Email:   </label><br/>
+       <input type="email" name="email" />
+       <input type="submit" className="submit" value="Login"/>
+     </form>
+  </div>
+  </>
+  )
+}
+
+export default Login;

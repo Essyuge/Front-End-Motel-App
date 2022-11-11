3a28cea3ed0f533e2bfb1873cf896e0b391f74b6
Essyuge <ugeesther@gmail.com>
2022-11-11 12:12:53 +0300

    add rentals

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import { useEffect,useState } from "react";
+import { BrowserRouter as Router, Routes, Route,Link} from "react-router-dom";
+import Rentals from "./Rentals";
+import Login from "./Login";
+import Signup from "./Signup";
+import OwnerRental from "./OwnerRental";
+import AddRentals from "./AddRentals";
+import Update from "./Update";
+import Footer from "./Footer";

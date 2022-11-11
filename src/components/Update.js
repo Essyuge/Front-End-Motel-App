f6bd7f81fc6f72e9181f354110cd16ecaff8216e
Essyuge <ugeesther@gmail.com>
2022-11-11 12:20:10 +0300

    updates

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,12 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from "./components/App";
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import {useNavigate,useParams} from "react-router-dom";
+import { useState,useEffect } from "react";

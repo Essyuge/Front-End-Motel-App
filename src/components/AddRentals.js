475f8cae320d3fafcbb748b81e6f6889490ea27e
Essyuge <ugeesther@gmail.com>
2022-11-11 12:07:55 +0300

    updates

--- src/components/AddRentals.js
+++ src/components/AddRentals.js
@@ -1,4 +1,21 @@
 import {useNavigate,useParams} from "react-router-dom";
 import { useState } from "react";
 import {WiDirectionLeft} from 'react-icons/wi'
-   
+  
+function AddRentals({handleAdd}) {
+
+    const{owner_id}=useParams()
+  
+    const [add, setadd] = useState({available:true,
+     owner_id:parseInt(owner_id)
+    });
+    const navigate=useNavigate()
+  
+    function onchange(e){
+      const { name, value } = e.target;
+      setadd({
+        ...add,
+        [name]: value
+      });
+    
+    }
